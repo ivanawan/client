8050f22e56bcb14ba7b2987f8068766709b72455
ivansetiawan <ivansetiawanvll@gmail.com>
2022-07-08 09:37:36 +0700

    init

--- src/page/ListTransaction .js
+++ src/page/ListTransaction .js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
+import axios from '../component/axios';
+import { check } from '../component/check';
 import Layout from './layout/Layout';
 
 
 const ListTransaction = () => {
+  const user = useSelector((state)=>state.user);
+  axios.defaults.headers.common['Authorization'] = `Bearer ${user.token}`;
+  const {data}=useQuery('transaction', async ()=>{
+    return await axios.get("/transaction");
+  });
+check(data);
+
+function textcolor(item){
+if(item ==="Approve"){
+  return "text-green-400";
+}
+
+if(item === "Cancel"){
+  return "text-rose-500";
+}
+
+if(item === "Pending"){
+  return "text-yellow-500";
+}
+}
+
     return (
         <Layout>
             <div className=' flex justify-center min-h-[78vh]'>
@@ -23,13 +48,16 @@ const ListTransaction = () => {
       </tr>
     </thead>
     <tbody>
-    
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
+    {data?.data.data.transactions.map(item=>
+      <tr key={item.id}>
+        <th>{item.id}</th>
+        <td>{item.User.fullName}</td>
+        <td className=' text-sky-600'>{item.attachment}</td>
+        <td>{item.Purchases[0].Book.title}</td>
+        <td className={item.status === "Approve" ? "text-green-400":" text-rose-500"}>Rp. {item.totalPayment}</td>
+        <td className={textcolor(item.status)}>{item.status}</td>
       </tr>
+        )}
      
   
     
